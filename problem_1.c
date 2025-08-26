4ffe79834d67efb2844092bcfa5c53cee5083d48
nakib233 <nakibulhasansadik23@gmail.com>
2025-08-26 14:27:50 +0600

    first commit

---
+++ problem_1.c
@@ -0,0 +1,15 @@
+
+// Problem_1.1
+
+#include <stdio.h>
+int main() {
+    int basic_sal;
+    printf("Enter Basic Salary of Ramesh: ");
+    scanf("%d", &basic_sal);
+    int da = basic_sal * .4;
+    int hr = basic_sal * .2;
+
+    float TgSalary = basic_sal + da + hr;
+    printf("The total gross Salary: %0.2f", TgSalary);
+    return 0;
+}
